914dcb63f0e859222cdca7f4bfbc30309095d179
emonShimoul <emonshimoul@gmail.com>
2024-08-19 19:33:24 +0600

    adding and deleting tasks

---
+++ todo-list/script.js
@@ -0,0 +1,29 @@
+document.querySelector('#push').onclick = function(){
+
+    // adding validation for empty input field
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task");
+    }
+    else{
+        // adding a task
+        document.querySelector('#tasks').innerHTML += 
+        `
+        <div class="task">
+            <span id="taskname">
+                ${document.querySelector('#newtask input').value}
+            </span>
+            <button class="delete">
+                <i class="far fa-trash-alt"></i>
+            </button>
+        </div>
+        `;
+
+        // deleting a task
+        const current_tasks = document.querySelectorAll(".delete");
+        for(let i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    }
+}
