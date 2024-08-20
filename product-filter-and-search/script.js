0518bb7b59262f301faf996a22f3080e1f00ca95
emonShimoul <emonshimoul@gmail.com>
2024-08-20 11:39:54 +0600

    display the products using js

---
+++ product-filter-and-search/script.js
@@ -0,0 +1,69 @@
+let products = {
+    data: [
+      {
+        productName: "Regular White T-Shirt",
+        category: "Topwear",
+        price: "30",
+        image: "white-tshirt.jpg",
+      },
+      {
+        productName: "Beige Short Skirt",
+        category: "Bottomwear",
+        price: "49",
+        image: "short-skirt.jpg",
+      },
+      {
+        productName: "Sporty SmartWatch",
+        category: "Watch",
+        price: "99",
+        image: "sporty-smartwatch.jpg",
+      },
+      {
+        productName: "Basic Knitted Top",
+        category: "Topwear",
+        price: "29",
+        image: "knitted-top.jpg",
+      },
+      {
+        productName: "Black Leather Jacket",
+        category: "Jacket",
+        price: "129",
+        image: "black-leather-jacket.jpg",
+      },
+      {
+        productName: "Stylish Pink Trousers",
+        category: "Bottomwear",
+        price: "89",
+        image: "pink-trousers.jpg",
+      },
+      {
+        productName: "Brown Men's Jacket",
+        category: "Jacket",
+        price: "189",
+        image: "brown-jacket.jpg",
+      },
+      {
+        productName: "Comfy Gray Pants",
+        category: "Bottomwear",
+        price: "49",
+        image: "comfy-gray-pants.jpg",
+      },
+    ],
+  };
+
+  for(let i of products.data){
+    //create card
+    let card = document.createElement("div");
+    // card should have category
+    card.classList.add("card", "i.category");
+    // image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    // img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+
+    document.getElementById("products").appendChild(card);
+  }
