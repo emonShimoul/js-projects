0c4f2d2dc9a3b9255ec65bac1bd0bfa8038d53d1
emonShimoul <emonshimoul@gmail.com>
2024-08-21 16:03:49 +0600

    final submission

--- random-joke-generator/script.js
+++ random-joke-generator/script.js
@@ -3,13 +3,13 @@ const btn = document.getElementById("btn");
 const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 
 let getJoke = () => {
-    // jokeContainer.classList.remove("fade");
+    jokeContainer.classList.remove("faded");
     fetch(url)
     .then(data => data.json())
     .then(item => {
         console.log(item.joke);
         jokeContainer.textContent = `${item.joke}`;
-        // jokeContainer.classList.add("fade");
+        jokeContainer.classList.add("faded");
     },[]);
 }
 btn.addEventListener("click",getJoke);
