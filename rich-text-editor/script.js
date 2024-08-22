fbc25caa2c40cadfbe5ce8ea86c5a49276e258c1
emonShimoul <emonshimoul@gmail.com>
2024-08-22 14:31:44 +0600

    highlight clicked button

--- rich-text-editor/script.js
+++ rich-text-editor/script.js
@@ -29,3 +29,32 @@ const initializer = () => {
     highlighter(formatButtons, false);
     highlighter(scriptButtons, true);
 }
+
+// Highlight clicked button
+const highlighter = (className, needsRemoval) => {
+    className.forEach((button) => {
+        button.addEventListener("click", () => {
+            // needsRemoval = true means only one button should be highlight and other would be normal
+            if(needsRemoval){
+                let alreadyActive = false;
+
+                // If currently clicked button is already active
+                if(button.classList.contains("active")){
+                    alreadyActive = true;
+                }
+
+                //  Remove highlight from other buttons
+                highlighterRemover(className);
+                if(!alreadyActive){
+                    // highlight clicked button
+                    button.classList.add("active");
+                }
+            } else{
+                // if other buttons can be highlighted
+                button.classList.toggle("active");
+            }
+        });
+    });
+};
+
+window.onload = initializer();
