76da3cfbbf04f0125364922e65e35641bba648af
emonShimoul <emonshimoul@gmail.com>
2024-08-23 21:38:05 +0600

    reusable function for similar tasks

--- geometry-genius/scripts/area-calculate.js
+++ geometry-genius/scripts/area-calculate.js
@@ -35,3 +35,25 @@ function calculateRectangleArea() {
   const rectangleAreaSpan = document.getElementById("rectangle-area");
   rectangleAreaSpan.innerText = area;
 }
+
+function calculateParallelogramArea() {
+  const base = getInputValueById("parallelogram-base");
+  const height = getInputValueById("parallelogram-height");
+
+  const area = base * height;
+
+  setInnerTextById("parallelogram-area", area);
+}
+
+function getInputValueById(inputFieldId) {
+  const inputField = document.getElementById(inputFieldId);
+  const inputValueText = inputField.value;
+  const inputValue = parseFloat(inputValueText);
+  //   console.log(inputValue);
+  return inputValue;
+}
+
+function setInnerTextById(elementId, area) {
+  const element = document.getElementById(elementId);
+  element.innerText = area;
+}
