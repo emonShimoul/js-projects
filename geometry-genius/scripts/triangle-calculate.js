151ca45df1859aea1d008cfb0be56ab2056eaee3
emonShimoul <emonshimoul@gmail.com>
2024-08-23 18:57:46 +0600

    get triangle base and height using event and function

--- geometry-genius/scripts/triangle-calculate.js
+++ geometry-genius/scripts/triangle-calculate.js
@@ -1,3 +1,13 @@
 function calculateTriangleArea() {
-  console.log("Inside Function");
+  // get triangle base value
+  const triangleBaseInput = document.getElementById("triangle-base");
+  const triangleBaseText = triangleBaseInput.value;
+  const base = parseFloat(triangleBaseText);
+  console.log(base);
+
+  // get triangle height value
+  const triangleHeightInput = document.getElementById("triangle-height");
+  const triangleHeightText = triangleHeightInput.value;
+  const height = parseFloat(triangleHeightText);
+  console.log(height);
 }
