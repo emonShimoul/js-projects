23ecc68a4eee149f6acf649bfeb71b3204465529
emonShimoul <emonshimoul@gmail.com>
2024-08-24 02:41:03 +0600

    added pentagon calculation

--- geometry-genius/scripts/area-calculate.js
+++ geometry-genius/scripts/area-calculate.js
@@ -45,6 +45,15 @@ function calculateParallelogramArea() {
   setInnerTextById("parallelogram-area", area);
 }
 
+function calculatePentagonArea() {
+  const perimeter = getInputValueById("pentagon-perimeter");
+  const apothem = getInputValueById("pentagon-apothem");
+
+  const area = 0.5 * perimeter * apothem;
+
+  setInnerTextById("pentagon-area", area);
+}
+
 function getInputValueById(inputFieldId) {
   const inputField = document.getElementById(inputFieldId);
   const inputValueText = inputField.value;
