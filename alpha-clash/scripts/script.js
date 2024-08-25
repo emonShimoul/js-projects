839bd0920557a390d191e2b174804556f447f203
emonShimoul <emonshimoul@gmail.com>
2024-08-25 11:50:58 +0600

    show and hide section

--- alpha-clash/scripts/script.js
+++ alpha-clash/scripts/script.js
@@ -1,3 +1,9 @@
 function play() {
-  console.log("play");
+  // hide the home screen
+  const homeSection = document.getElementById("home-screen");
+  homeSection.classList.add("hidden");
+
+  // show the playground
+  const playGroundSection = document.getElementById("play-ground");
+  playGroundSection.classList.remove("hidden");
 }
