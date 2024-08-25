9bf295919aaa54b75b502458989f36a6c17de3ce
emonShimoul <emonshimoul@gmail.com>
2024-08-25 21:47:15 +0600

    some addition

--- alpha-clash/scripts/utility.js
+++ alpha-clash/scripts/utility.js
@@ -13,6 +13,11 @@ function setBGColorById(elementId) {
   element.classList.add("bg-orange-400");
 }
 
+function removeBGColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("bg-orange-400");
+}
+
 function getARandomAlphabet() {
   // get or create an alphabet array
   const alphabetString = "abcdefghijklmnopqrstuvwxyz";
