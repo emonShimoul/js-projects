64c769e3dbc6875af5cd7b83139b5bd8506c179a
emonShimoul <emonshimoul@gmail.com>
2024-08-25 11:56:45 +0600

    show and hide section using common function

--- alpha-clash/scripts/script.js
+++ alpha-clash/scripts/script.js
@@ -1,9 +1,14 @@
-function play() {
-  // hide the home screen
-  const homeSection = document.getElementById("home-screen");
-  homeSection.classList.add("hidden");
+// function play() {
+//   // hide the home screen
+//   const homeSection = document.getElementById("home-screen");
+//   homeSection.classList.add("hidden");
 
-  // show the playground
-  const playGroundSection = document.getElementById("play-ground");
-  playGroundSection.classList.remove("hidden");
+//   // show the playground
+//   const playGroundSection = document.getElementById("play-ground");
+//   playGroundSection.classList.remove("hidden");
+// }
+
+function play() {
+  hideElementById("home-screen");
+  showElementById("play-ground");
 }
