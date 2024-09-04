eebf6e38622cf64934b8d160a05c90f8fa7f00d7
emonShimoul <emonshimoul@gmail.com>
2024-09-04 18:21:27 +0600

    seat counter code implemented

--- smart-ticketing-system/scripts/script.js
+++ smart-ticketing-system/scripts/script.js
@@ -7,5 +7,14 @@ for (let seat of seatButton) {
     seat.classList.toggle("btn-selected");
     // seat.classList.add("cursor-not-allowed");
     // seat.classList.remove("text-gray-500");
+    let selectedSeatCounter = document.getElementById("selected-seat-counter");
+    let selectedSeatCounterInt = parseInt(selectedSeatCounter.innerText);
+    if (seat.classList.contains("btn-selected")) {
+      selectedSeatCounterInt += 1;
+      selectedSeatCounter.innerText = selectedSeatCounterInt;
+    } else {
+      selectedSeatCounterInt -= 1;
+      selectedSeatCounter.innerText = selectedSeatCounterInt;
+    }
   });
 }
