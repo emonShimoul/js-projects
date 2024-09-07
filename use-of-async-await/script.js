448136fdca539b6d581292356a71fa49e7bc093e
emonShimoul <emonshimoul@gmail.com>
2024-09-07 13:00:51 +0600

    additional code

--- use-of-async-await/script.js
+++ use-of-async-await/script.js
@@ -1,5 +1,12 @@
 const loadComments = () => {
   fetch("https://jsonplaceholder.typicode.com/comments")
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => console.log(data))
+    .catch((error) => console.error("error happened", error));
+};
+
+const loadComments2 = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const data = await res.json();
+  console.log(data);
 };
