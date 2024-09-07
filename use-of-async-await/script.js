dcd27d19823a866499fac25d43f1720706b07527
emonShimoul <emonshimoul@gmail.com>
2024-09-07 13:02:32 +0600

    additional code using try catch block with async await

--- use-of-async-await/script.js
+++ use-of-async-await/script.js
@@ -6,7 +6,11 @@ const loadComments = () => {
 };
 
 const loadComments2 = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/comments");
-  const data = await res.json();
-  console.log(data);
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.error("data load error");
+  }
 };
