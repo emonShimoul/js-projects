299a9beaed10a41d23688020c84d84e6b811bde2
emonShimoul <emonshimoul@gmail.com>
2024-09-08 22:07:18 +0600

    display the phone data in the UI

--- hunting-phone-api/js/script.js
+++ hunting-phone-api/js/script.js
@@ -9,9 +9,30 @@ const loadPhone = async () => {
 };
 
 const displayPhones = (phones) => {
-  console.log(phones);
+  // console.log(phones);
+  const phoneContainer = document.getElementById("phone-container");
   phones.forEach((phone) => {
     console.log(phone);
+    // 1. create a div
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card bg-gray-100 w-96 shadow-xl`;
+    phoneCard.innerHTML = `
+    <figure>
+      <img
+        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+        alt="Shoes"
+      />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">Shoes!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
+    `;
+
+    phoneContainer.appendChild(phoneCard);
   });
 };
 
