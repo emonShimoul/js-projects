0e588531f975302d3bd84f63551491464c123415
emonShimoul <emonshimoul@gmail.com>
2024-09-09 14:18:41 +0600

    implementing show details button

--- hunting-phone-api/js/script.js
+++ hunting-phone-api/js/script.js
@@ -41,8 +41,8 @@ const displayPhones = (phones, isShowAll) => {
     <div class="card-body">
       <h2 class="card-title">${phone.phone_name}</h2>
       <p>If a dog chews shoes whose shoes does he choose?</p>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+      <div class="card-actions justify-center">
+        <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
       </div>
     </div>
     `;
@@ -52,6 +52,10 @@ const displayPhones = (phones, isShowAll) => {
 
   // hide loading spinner
   toggleLoadingSpinner(false);
+};
+
+const handleShowDetails = (id) => {
+  console.log("clicked show details", id);
 };
 
 // handle search button
