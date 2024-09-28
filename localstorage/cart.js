ae4dd3a32f2fac0ddaa8864b4385de4568a4ff85
emonShimoul <emonshimoul@gmail.com>
2024-09-28 17:25:51 +0600

    local storage addition

--- localstorage/cart.js
+++ localstorage/cart.js
@@ -8,4 +8,12 @@ const addProduct = () => {
   quantityField.value = "";
 
   console.log(product, quantity);
+  displayProduct(product, quantity);
+};
+
+const displayProduct = (product, quantity) => {
+  const ul = document.getElementById("product-container");
+  const li = document.createElement("li");
+  li.innerText = `${product}: ${quantity}`;
+  ul.appendChild(li);
 };
