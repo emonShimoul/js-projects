865be2b945ea8f7d02afd16ed6bb943a0843adec
emonShimoul <emonshimoul@gmail.com>
2024-09-28 16:32:07 +0600

    event loop example

--- event-loop/event-loop.js
+++ event-loop/event-loop.js
@@ -28,6 +28,12 @@ function z() {
   console.log("z");
 }
 
+setTimeout =
+  (() => {
+    console.log("inside timeout");
+  },
+  2000);
+
 fetch("https://jsonplaceholder.typicode.com/todos/1")
   .then((response) => response.json())
   .then((json) => console.log(json));
