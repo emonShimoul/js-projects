c1e556d1eb175167b35d95ad0148b789547c7e8e
emonShimoul <emonshimoul@gmail.com>
2024-12-20 11:26:21 +0600

    start a new project

--- Payoo-MFS/js/features.js
+++ Payoo-MFS/js/features.js
@@ -1,35 +1,9 @@
-document
-  .getElementById("btn-show-add-money")
-  .addEventListener("click", function () {
-    showSectionById("add-money-form");
-  });
+document.getElementById("donation-btn").addEventListener("click", function () {
+  showSectionById("add-section");
+  showButtonById("donation-btn");
+});
 
-document
-  .getElementById("btn-show-cash-out")
-  .addEventListener("click", function () {
-    showSectionById("cash-out-form");
-  });
-
-document
-  .getElementById("btn-transaction")
-  .addEventListener("click", function () {
-    showSectionById("transaction-section");
-  });
-
-// document
-//   .getElementById("btn-show-cash-out")
-//   .addEventListener("click", function () {
-//     console.log("show cash out button clicked");
-//     document.getElementById("cash-out-form").classList.remove("hidden");
-
-//     document.getElementById("add-money-form").classList.add("hidden");
-//   });
-
-// document
-//   .getElementById("btn-show-add-money")
-//   .addEventListener("click", function () {
-//     console.log("show cash out button clicked");
-//     document.getElementById("cash-out-form").classList.add("hidden");
-
-//     document.getElementById("add-money-form").classList.remove("hidden");
-//   });
+document.getElementById("history-btn").addEventListener("click", function () {
+  showSectionById("transaction-container");
+  showButtonById("history-btn");
+});
