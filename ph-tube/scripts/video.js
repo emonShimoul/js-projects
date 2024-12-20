205337d2f57d8378d81539676a725e4098030a23
emonShimoul <emonshimoul@gmail.com>
2024-12-21 03:47:21 +0600

    load videos and display cards on the UI

--- ph-tube/scripts/video.js
+++ ph-tube/scripts/video.js
@@ -8,6 +8,38 @@ const loadCategories = () => {
     .catch((error) => console.log(error));
 };
 
+const loadVideos = () => {
+  console.log("Loadcategories created!!");
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+};
+
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("videos");
+  videos.forEach((video) => {
+    console.log(video);
+    const card = document.createElement("div");
+    card.classList = "card card-compact";
+    card.innerHTML = `
+    <figure>
+        <img
+        src=${video.thumbnail}
+        alt="Shoes" />
+    </figure>
+    <div class="card-body">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+        </div>
+    </div>
+    `;
+    videoContainer.append(card);
+  });
+};
+
 const displayCategories = (categories) => {
   const categoryContainer = document.getElementById("categories");
 
@@ -24,3 +56,4 @@ const displayCategories = (categories) => {
 };
 
 loadCategories();
+loadVideos();
